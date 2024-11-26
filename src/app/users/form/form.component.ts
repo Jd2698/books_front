f86a9acb2f36d9ce6bc1d627b41e90044fad833c
Jamer Delgado <jamer.delgado@dreamcodesoft.com>
2024-11-26 14:43:47 -0500

    update user component

--- src/app/users/form/form.component.ts
+++ src/app/users/form/form.component.ts
@@ -1,185 +1,169 @@
-import { NgFor, NgIf } from '@angular/common'
+import { NgFor, NgIf } from '@angular/common';
 import {
-	Component,
-	EventEmitter,
-	inject,
-	Input,
-	OnInit,
-	Output
-} from '@angular/core'
+  Component,
+  EventEmitter,
+  inject,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
 import {
-	FormBuilder,
-	FormGroup,
-	ReactiveFormsModule,
-	Validators
-} from '@angular/forms'
-import { FileUploadModule } from 'primeng/fileupload'
-import { ButtonModule } from 'primeng/button'
-import { UsersService } from '../services/users.service'
-import { noWhitespaceValidator } from '../../shared/whitespace.validator'
-import { Iuser } from '../model/user.model'
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FileUploadModule } from 'primeng/fileupload';
+import { ButtonModule } from 'primeng/button';
+import { UsersService } from '../services/users.service';
+import { noWhitespaceValidator } from '../../shared/whitespace.validator';
+import { Iuser } from '../model/user.model';
 
 @Component({
-	selector: 'app-form',
-	standalone: true,
-	imports: [ButtonModule, ReactiveFormsModule, NgIf, FileUploadModule],
-	templateUrl: './form.component.html',
-	styleUrl: './form.component.css'
+  selector: 'app-form',
+  standalone: true,
+  imports: [ButtonModule, ReactiveFormsModule, NgIf, FileUploadModule],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css',
 })
 export class FormComponent implements OnInit {
-	@Input() selectedUser!: Iuser
+  @Input() selectedUser!: any;
 
-	@Output() toggleModalVisibility = new EventEmitter<void>()
-	@Output() refreshUsers = new EventEmitter<void>()
-	@Output() showToast = new EventEmitter<{
-		severity: string
-		summary: string
-		details: string
-		life: number
-	}>()
+  @Output() toggleModalVisibility = new EventEmitter<void>();
+  @Output() refreshUsers = new EventEmitter<void>();
+  @Output() showToast = new EventEmitter<{
+    severity: string;
+    summary: string;
+    details: string;
+    life: number;
+  }>();
 
-	formData = new FormData()
-	imageSrc: string | ArrayBuffer | null = null
-	formGroup!: FormGroup
-	private _userService = inject(UsersService)
-	private _formBuild = inject(FormBuilder)
+  formData = new FormData();
+  imageSrc: string | ArrayBuffer | null = null;
+  formGroup!: FormGroup;
+  private _userService = inject(UsersService);
+  private _formBuild = inject(FormBuilder);
 
-	ngOnInit(): void {
-		this.initializeForm()
+  ngOnInit(): void {
+    this.initializeForm();
 
-		if (this.selectedUser) {
-			this.formGroup.setValue({
-				nombre: this.selectedUser.nombre,
-				email: this.selectedUser.email,
-				telefono: this.selectedUser.telefono
-			})
-		}
-	}
+    if (this.selectedUser) {
+      this.formGroup.setValue({
+        nombre: this.selectedUser.nombre,
+        email: this.selectedUser.email,
+        telefono: this.selectedUser.telefono,
+      });
+    }
+  }
 
-	initializeForm(): void {
-		this.formGroup = this._formBuild.group({
-			nombre: ['', [Validators.required, noWhitespaceValidator()]],
-			email: ['', [Validators.required, Validators.email]],
-			telefono: [null, Validators.pattern(/^\d{10}$/)]
-		})
-	}
+  initializeForm(): void {
+    this.formGroup = this._formBuild.group({
+      nombre: ['', [Validators.required, noWhitespaceValidator()]],
+      email: ['', [Validators.required, Validators.email]],
+      telefono: [null, Validators.pattern(/^\d{10}$/)],
+    });
+  }
 
-	emitModalToggle(): void {
-		this.toggleModalVisibility.emit()
-	}
+  emitModalToggle(): void {
+    this.toggleModalVisibility.emit();
+  }
 
-	hasError(data: { field: string; error: string }): boolean {
-		const control = this.formGroup.controls[data.field]
+  hasError(data: { field: string; error: string }): boolean {
+    const control = this.formGroup.controls[data.field];
 
-		const error = control.errors?.[data.error]
-		const isTouchedOrDirty = control.touched || control.dirty
+    const error = control.errors?.[data.error];
+    const isTouchedOrDirty = control.touched || control.dirty;
 
-		return error && isTouchedOrDirty
-	}
+    return error && isTouchedOrDirty;
+  }
 
-	resetFormGroup(): void {
-		this.formGroup.reset()
-	}
+  resetFormGroup(): void {
+    this.formGroup.reset();
+  }
 
-	changeFile(event: any) {
-		const file = event.target.files[0]
-		if (this.formData.has('file')) this.formData.delete('file')
-		this.formData.append('file', file, file.name)
+  changeFile(event: any) {
+    const file = event.target.files[0];
+    if (this.formData.has('file')) this.formData.delete('file');
+    this.formData.append('file', file, file.name);
 
-		const reader = new FileReader()
+    const reader = new FileReader();
 
-		reader.onload = () => {
-			this.imageSrc = reader.result
-		}
+    reader.onload = () => {
+      this.imageSrc = reader.result;
+    };
 
-		reader.readAsDataURL(file)
-	}
+    reader.readAsDataURL(file);
+  }
 
-	submit(): void {
-		const telefono = this.formGroup.controls['telefono'].value.trim() || null
-		const data = { ...this.selectedUser, ...this.formGroup.value, telefono }
+  submit(): void {
+    const formValues = this.formGroup.value;
+    let data = { ...formValues };
 
-		//para actualizar y crear
-		if (this.selectedUser) {
-			this._userService.update(this.selectedUser.id, data).subscribe({
-				next: response => {
-					this.resetFormGroup()
-					this.refreshUsers.emit()
-					this.emitModalToggle()
+    //para actualizar y crear
+    if (this.selectedUser) {
+      for (const key in formValues) {
+        if (formValues.hasOwnProperty(key)) {
+          this.formData.append(key, formValues[key]);
+        }
+      }
 
-					this.showToast.emit({
-						severity: 'success',
-						summary: 'user updated',
-						details: 'The user has been updated.',
-						life: 3000
-					})
-				},
-				error: error => {
-					console.error('Error updating user', error)
-					this.showToast.emit({
-						severity: 'error',
-						summary: 'Error updating user.',
-						details: 'The user could not be updated.',
-						life: 3000
-					})
-				}
-			})
-		} else {
-			if (!this.formData.has('file')) {
-				this._userService.create({ ...data }).subscribe({
-					next: response => {
-						this.resetFormGroup()
-						this.refreshUsers.emit()
-						this.emitModalToggle()
+      this._userService.update(this.selectedUser.id, data).subscribe({
+        next: (response) => {
+          this.resetFormGroup();
+          this.refreshUsers.emit();
+          this.emitModalToggle();
 
-						this.showToast.emit({
-							severity: 'success',
-							summary: 'New user added',
-							details: 'The user has been added.',
-							life: 3000
-						})
-					},
-					error: error => {
-						console.error('Error fetching users', error)
-						this.showToast.emit({
-							severity: 'error',
-							summary: 'Error creating user.',
-							details: 'The user could not be created.',
-							life: 3000
-						})
-					}
-				})
-			} else {
-				this._userService.uploadImage(this.formData).subscribe({
-					next: response => {
-						this._userService
-							.create({ ...data, imagen: response.filename })
-							.subscribe({
-								next: response => {
-									this.resetFormGroup()
-									this.refreshUsers.emit()
-									this.emitModalToggle()
+          this.showToast.emit({
+            severity: 'success',
+            summary: 'user updated',
+            details: 'The user has been updated.',
+            life: 3000,
+          });
+        },
+        error: (error) => {
+          console.error('Error updating user', error);
+          this.showToast.emit({
+            severity: 'error',
+            summary: 'Error updating user.',
+            details: 'The user could not be updated.',
+            life: 3000,
+          });
+        },
+      });
+    } else {
+      for (const key in formValues) {
+        if (formValues.hasOwnProperty(key)) {
+          console.log(key);
 
-									this.showToast.emit({
-										severity: 'success',
-										summary: 'New user added',
-										details: 'The user has been added.',
-										life: 3000
-									})
-								},
-								error: error => {
-									console.error('Error fetching users', error)
-									this.showToast.emit({
-										severity: 'error',
-										summary: 'Error creating user.',
-										details: 'The user could not be created.',
-										life: 3000
-									})
-								}
-							})
-					}
-				})
-			}
-		}
-	}
+          this.formData.append(key, formValues[key]);
+        }
+	  }
+		
+		return
+
+      this._userService.create(this.formData).subscribe({
+        next: (response) => {
+          this.resetFormGroup();
+          this.refreshUsers.emit();
+          this.emitModalToggle();
+
+          this.showToast.emit({
+            severity: 'success',
+            summary: 'New user added',
+            details: 'The user has been added.',
+            life: 3000,
+          });
+        },
+        error: (error) => {
+          console.error('Error fetching users', error);
+          this.showToast.emit({
+            severity: 'error',
+            summary: 'Error creating user.',
+            details: 'The user could not be created.',
+            life: 3000,
+          });
+        },
+      });
+    }
+  }
 }
--- src/app/users/services/users.service.ts
+++ src/app/users/services/users.service.ts
@@ -1,31 +1,31 @@
-import { HttpClient } from '@angular/common/http'
-import { Injectable } from '@angular/core'
-import { Observable } from 'rxjs'
-import { Iuser } from '../model/user.model'
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Iuser } from '../model/user.model';
 @Injectable({
-	providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersService {
-	constructor(private _http: HttpClient) {}
-	urlPath: string = 'http://localhost:3000/users'
+  constructor(private _http: HttpClient) {}
+  urlPath: string = 'http://localhost:3000/users';
 
-	uploadImage(data:any): Observable<any> {
-		return this._http.post(`${this.urlPath}/upload`, data)
-	}
+  uploadImage(data: any): Observable<any> {
+    return this._http.post(`${this.urlPath}/upload`, data);
+  }
 
-	getAll(): Observable<Iuser[]> {
-		return this._http.get<Iuser[]>(this.urlPath)
-	}
+  getAll(): Observable<Iuser[]> {
+    return this._http.get<Iuser[]>(this.urlPath);
+  }
 
-	create(data: Iuser): Observable<Iuser> {
-		return this._http.post<Iuser>(this.urlPath, data)
-	}
+  create(data: FormData): Observable<Iuser> {
+    return this._http.post<Iuser>(this.urlPath, data);
+  }
 
-	delete(userId: number): Observable<Iuser> {
-		return this._http.delete<Iuser>(`${this.urlPath}/${userId}`)
-	}
+  delete(userId: number): Observable<Iuser> {
+    return this._http.delete<Iuser>(`${this.urlPath}/${userId}`);
+  }
 
-	update(userId: number, data: Iuser): Observable<Iuser> {
-		return this._http.put<Iuser>(`${this.urlPath}/${userId}`, data)
-	}
+  update(userId: number, data: Iuser): Observable<Iuser> {
+    return this._http.put<Iuser>(`${this.urlPath}/${userId}`, data);
+  }
 }
